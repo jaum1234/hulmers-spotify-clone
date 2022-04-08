275e17a956a7aa9fb2e68b844d39ccf13ada003a
jaum1234 <joaovitorsouzacoura@gmail.com>
2022-04-08 09:03:20 -0300

    commit

--- music-app/src/services/store/index.ts
+++ music-app/src/services/store/index.ts
@@ -2,6 +2,7 @@ import { createStore } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import rootReducer from './reducers';
 import storage from "redux-persist/lib/storage";
+import { createWrapper } from "next-redux-wrapper";
 
 const persistConfig = {
     key: 'hulmers',
@@ -12,5 +13,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = createStore(persistedReducer);
 const persistor = persistStore(store);
+
+const storeWrapper = createWrapper
 
 export { store, persistor };
